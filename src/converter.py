b24d249111fa627f4ecf6d8f0dbac1f622ec79ed
kryzdof <40175916+kryzdof@users.noreply.github.com>
2026-02-08 04:31:33 +0100

    converters done

---
+++ src/converter.py
@@ -0,0 +1,96 @@
+import re
+from textnode import TextNode, TextType
+
+def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            if node.text.count(delimiter) % 2:
+                raise LookupError(f"Opening and Closing delimiter ({delimiter}) missmatch")
+            splitted = node.text.split(delimiter)
+            for i, s in enumerate(splitted):
+                if i % 2:
+                    new_nodes.append(TextNode(s, text_type))
+                else:
+                    new_nodes.append(TextNode(s, TextType.TEXT))
+    return new_nodes
+
+
+# links:  [anchor text](link url) (?<!\!)\[.+?\]\(.+?\)
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    pattern = r"(?<!\!)\[.+?\]\(.+?\)"
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            links = re.findall(pattern, node.text)
+            if links:
+                text = node.text
+                for link in links:
+                    # string slice because we want to get rid of the first [ and the last )
+                    alt, src = link[1:-1].split("](")
+                    link_node = TextNode(alt, TextType.LINK, src)
+                    left, right = re.search(pattern, text).span()
+                    if left == 0:
+                        # link is at the beginning of the text -> no text node needed
+                        new_nodes.append(link_node)
+                    else:
+                        new_nodes.append(TextNode(text[:left], TextType.TEXT))
+                        new_nodes.append(link_node)
+                    text = text[right:]
+                if text:
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+            else:
+                new_nodes.append(node)
+    return new_nodes
+
+
+# images: ![alt text](image url) !\[.+?\]\(.+?\)
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    pattern = r"!\[.+?\]\(.+?\)"
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            images = re.findall(pattern, node.text)
+            if images:
+                text = node.text
+                for image in images:
+                    # string slice because we want to get rid of the first ![ and the last )
+                    alt, src = image[2:-1].split("](")
+                    image_node = TextNode(alt, TextType.IMAGE, src)
+                    left, right = re.search(pattern, text).span()
+                    if left == 0:
+                        # image is at the beginning of the text -> no text node needed
+                        new_nodes.append(image_node)
+                    else:
+                        new_nodes.append(TextNode(text[:left], TextType.TEXT))
+                        new_nodes.append(image_node)
+                    text = text[right:]
+                if text:
+                    new_nodes.append(TextNode(text, TextType.TEXT))
+            else:
+                new_nodes.append(node)
+    return new_nodes
+
+
+def text_to_textnodes(text: str) -> list[TextNode]:
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    return nodes
+
+
+def markdown_to_blocks(markdown):
+    blocks = []
+    for block in markdown.split("\n\n"):
+        if block.strip():
+            blocks.append(block.strip())
+    return blocks
---
+++ src/test_converter.py
@@ -0,0 +1,278 @@
+import unittest
+
+import textnode
+import converter
+
+
+class TestDelimiterConversion(unittest.TestCase):
+    def test_one_bold(self):
+        old_nodes = [textnode.TextNode("Ein text mit **fettem** Zeug", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_delimiter(old_nodes, "**", textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "fettem")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " Zeug")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)
+
+    def test_two_bold(self):
+        old_nodes = [textnode.TextNode("Ein **fetter** text mit **fettem** Zeug", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_delimiter(old_nodes, "**", textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[0].text, "Ein ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "fetter")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, " text mit ")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)       
+        self.assertEqual(new_nodes[3].text, "fettem")
+        self.assertEqual(new_nodes[3].text_type, textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[4].text, " Zeug")
+        self.assertEqual(new_nodes[4].text_type, textnode.TextType.TEXT)
+
+    def test_two_direct_bold(self):
+        old_nodes = [textnode.TextNode("Ein text mit **wirklich ****fettem** Zeug", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_delimiter(old_nodes, "**", textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "wirklich ")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, "")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)       
+        self.assertEqual(new_nodes[3].text, "fettem")
+        self.assertEqual(new_nodes[3].text_type, textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[4].text, " Zeug")
+        self.assertEqual(new_nodes[4].text_type, textnode.TextType.TEXT)
+
+    def test_two_direct_bold2(self):
+        old_nodes = [textnode.TextNode("Ein text mit **wirklich****fettem** Zeug", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_delimiter(old_nodes, "**", textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "wirklich")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[2].text, "")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)       
+        self.assertEqual(new_nodes[3].text, "fettem")
+        self.assertEqual(new_nodes[3].text_type, textnode.TextType.BOLD)
+        self.assertEqual(new_nodes[4].text, " Zeug")
+        self.assertEqual(new_nodes[4].text_type, textnode.TextType.TEXT)
+
+    def test_one_italic(self):
+        old_nodes = [textnode.TextNode("Ein text mit _fettem_ Zeug", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_delimiter(old_nodes, "_", textnode.TextType.ITALIC)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "fettem")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.ITALIC)
+        self.assertEqual(new_nodes[2].text, " Zeug")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)
+
+    def test_one_code(self):
+        old_nodes = [textnode.TextNode("Ein text mit `fettem` Zeug", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_delimiter(old_nodes, "`", textnode.TextType.CODE)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "fettem")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " Zeug")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)
+
+    def test_no_code(self):
+        old_nodes = [textnode.TextNode("Ein text mit fettem Zeug", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_delimiter(old_nodes, "`", textnode.TextType.CODE)
+        self.assertEqual(new_nodes[0].text, "Ein text mit fettem Zeug")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+
+    def test_exception(self):
+        old_nodes = [textnode.TextNode("Ein text mit `fettem Zeug", textnode.TextType.TEXT)]
+        with self.assertRaises(LookupError):
+            converter.split_nodes_delimiter(old_nodes, "`", textnode.TextType.CODE)
+
+
+class TestLinkConversion(unittest.TestCase):
+    def test_one_middle_link(self):
+        old_nodes = [textnode.TextNode("Ein text mit [einem link](https://www.link.at) in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_link(old_nodes)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "einem link")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.LINK)
+        self.assertEqual(new_nodes[1].url, "https://www.link.at")
+        self.assertEqual(new_nodes[2].text, " in der Mitte")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)
+
+    def test_one_start_link(self):
+        old_nodes = [textnode.TextNode("[einem link](https://www.link.at) in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_link(old_nodes)
+        self.assertEqual(new_nodes[0].text, "einem link")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.LINK)
+        self.assertEqual(new_nodes[0].url, "https://www.link.at")
+        self.assertEqual(new_nodes[1].text, " in der Mitte")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.TEXT)
+
+    def test_one_end_link(self):
+        old_nodes = [textnode.TextNode("Ein text mit [einem link](https://www.link.at)", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_link(old_nodes)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "einem link")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.LINK)
+        self.assertEqual(new_nodes[1].url, "https://www.link.at")
+
+    def test_two_middle_link(self):
+        old_nodes = [textnode.TextNode("Ein text mit [einem link](https://www.link.at) und [noch einem](https://www.link2.at/) in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_link(old_nodes)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "einem link")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.LINK)
+        self.assertEqual(new_nodes[1].url, "https://www.link.at")
+        self.assertEqual(new_nodes[2].text, " und ")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "noch einem")
+        self.assertEqual(new_nodes[3].text_type, textnode.TextType.LINK)
+        self.assertEqual(new_nodes[3].url, "https://www.link2.at/")
+        self.assertEqual(new_nodes[4].text, " in der Mitte")
+        self.assertEqual(new_nodes[4].text_type, textnode.TextType.TEXT)
+
+    def test_one_start_and_one_end_link(self):
+        old_nodes = [textnode.TextNode("[einem link](https://www.link.at) in der Mitte [noch einem](https://www.link2.at/)", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_link(old_nodes)
+        self.assertEqual(new_nodes[0].text, "einem link")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.LINK)
+        self.assertEqual(new_nodes[0].url, "https://www.link.at")
+        self.assertEqual(new_nodes[1].text, " in der Mitte ")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[2].text, "noch einem")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.LINK)
+        self.assertEqual(new_nodes[2].url, "https://www.link2.at/")
+
+    def test_no_link(self):
+        old_nodes = [textnode.TextNode("in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_link(old_nodes)
+        self.assertEqual(new_nodes[0].text, "in der Mitte")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+
+
+class TestImageConversion(unittest.TestCase):
+    def test_one_middle_image(self):
+        old_nodes = [textnode.TextNode("Ein text mit ![einem image](https://www.image.at) in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_image(old_nodes)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "einem image")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://www.image.at")
+        self.assertEqual(new_nodes[2].text, " in der Mitte")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)
+
+    def test_one_start_image(self):
+        old_nodes = [textnode.TextNode("![einem image](https://www.image.at) in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_image(old_nodes)
+        self.assertEqual(new_nodes[0].text, "einem image")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.IMAGE)
+        self.assertEqual(new_nodes[0].url, "https://www.image.at")
+        self.assertEqual(new_nodes[1].text, " in der Mitte")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.TEXT)
+
+    def test_one_end_image(self):
+        old_nodes = [textnode.TextNode("Ein text mit ![einem image](https://www.image.at)", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_image(old_nodes)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "einem image")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://www.image.at")
+
+    def test_two_middle_image(self):
+        old_nodes = [textnode.TextNode("Ein text mit ![einem image](https://www.image.at) und ![noch einem](https://www.image2.at/) in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_image(old_nodes)
+        self.assertEqual(new_nodes[0].text, "Ein text mit ")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "einem image")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.IMAGE)
+        self.assertEqual(new_nodes[1].url, "https://www.image.at")
+        self.assertEqual(new_nodes[2].text, " und ")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "noch einem")
+        self.assertEqual(new_nodes[3].text_type, textnode.TextType.IMAGE)
+        self.assertEqual(new_nodes[3].url, "https://www.image2.at/")
+        self.assertEqual(new_nodes[4].text, " in der Mitte")
+        self.assertEqual(new_nodes[4].text_type, textnode.TextType.TEXT)
+
+    def test_one_start_and_one_end_image(self):
+        old_nodes = [textnode.TextNode("![einem image](https://www.image.at) in der Mitte ![noch einem](https://www.image2.at/)", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_image(old_nodes)
+        self.assertEqual(new_nodes[0].text, "einem image")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.IMAGE)
+        self.assertEqual(new_nodes[0].url, "https://www.image.at")
+        self.assertEqual(new_nodes[1].text, " in der Mitte ")
+        self.assertEqual(new_nodes[1].text_type, textnode.TextType.TEXT)
+        self.assertEqual(new_nodes[2].text, "noch einem")
+        self.assertEqual(new_nodes[2].text_type, textnode.TextType.IMAGE)
+        self.assertEqual(new_nodes[2].url, "https://www.image2.at/")
+
+    def test_no_image(self):
+        old_nodes = [textnode.TextNode("in der Mitte", textnode.TextType.TEXT)]
+        new_nodes = converter.split_nodes_link(old_nodes)
+        self.assertEqual(new_nodes[0].text, "in der Mitte")
+        self.assertEqual(new_nodes[0].text_type, textnode.TextType.TEXT)
+
+
+class TestConversion(unittest.TestCase):
+    def test_example(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = converter.text_to_textnodes(text)
+        nodes2 = [
+            textnode.TextNode("This is ", textnode.TextType.TEXT),
+            textnode.TextNode("text", textnode.TextType.BOLD),
+            textnode.TextNode(" with an ", textnode.TextType.TEXT),
+            textnode.TextNode("italic", textnode.TextType.ITALIC),
+            textnode.TextNode(" word and a ", textnode.TextType.TEXT),
+            textnode.TextNode("code block", textnode.TextType.CODE),
+            textnode.TextNode(" and an ", textnode.TextType.TEXT),
+            textnode.TextNode("obi wan image", textnode.TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            textnode.TextNode(" and a ", textnode.TextType.TEXT),
+            textnode.TextNode("link", textnode.TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(nodes, nodes2)
+
+    def test_blocks(self):
+        markdown = """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+
+- This is the first list item in a list block
+- This is a list item
+- This is another list item
+"""
+        blocks = converter.markdown_to_blocks(markdown)
+        self.assertEqual(blocks[0], "# This is a heading")
+        self.assertEqual(blocks[1], "This is a paragraph of text. It has some **bold** and _italic_ words inside of it.")
+        self.assertEqual(blocks[2], """- This is the first list item in a list block
+- This is a list item
+- This is another list item""")
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = converter.markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+if __name__ == "__main__":
+    unittest.main()
